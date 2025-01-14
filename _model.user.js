311cf1d5bc208722ec3510c15a086d855b6543c3
xefiry <188508735+xefiry@users.noreply.github.com>
2025-01-14 20:07:25 +0100

    Adding _model

---
+++ _model.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        __name__
+// @version     1.0
+// @description __description__
+// @author      xefiry
+// @namespace   https://github.com/xefiry
+// @homepageURL https://github.com/xefiry/UserScripts
+// @supportURL  https://github.com/xefiry/UserScripts/issues
+// @downloadURL https://raw.githubusercontent.com/xefiry/UserScripts/master/__name__.user.js
+// @icon        http://xxxx/icon.png
+// @noframes
+// @run-at      document-end / document-idle
+// @grant       none
+// @match       *://example.org/*
+// ==/UserScript==
+
+console.log("Running");
