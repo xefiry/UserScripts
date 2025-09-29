19d2a4caf9c6398110d1784ac97ee7ffe30a3ba4
xefiry <188508735+xefiry@users.noreply.github.com>
2025-09-29 12:14:47 +0200

    jira__auto_refresh_dashboards fix

--- jira__auto_refresh_dashboards.user.js
+++ jira__auto_refresh_dashboards.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Jira - Auto refresh dashboards
-// @version     1.0
+// @version     1.1
 // @description Adds a timer to auto refresh dashboards.
 // @author      xefiry
 // @namespace   https://github.com/xefiry
@@ -14,68 +14,62 @@
 // @match       https://*.atlassian.net/jira/dashboards/*
 // ==/UserScript==
 
-var refresh_time = 5 * 60; // in seconds
+let refresh_time = 5 * 60 // in seconds
 
-var refresh_button;
-var refresh_text;
-var curent_time;
+let refresh_button
+let refresh_text
+let curent_time
 
 Number.prototype.toHHMMSS = function () {
-  var sec_num = parseInt(this, 10);
-  var hours   = Math.floor(sec_num / 3600);
-  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+  let sec_num = parseInt(this, 10)
+  let hours   = Math.floor(sec_num / 3600)
+  let minutes = Math.floor((sec_num - (hours * 3600)) / 60)
+  let seconds = sec_num - (hours * 3600) - (minutes * 60)
 
-  if (hours   < 10) {hours   = "0"+hours;}
-  if (minutes < 10) {minutes = "0"+minutes;}
-  if (seconds < 10) {seconds = "0"+seconds;}
+  if (hours   < 10) {hours   = "0"+hours}
+  if (minutes < 10) {minutes = "0"+minutes}
+  if (seconds < 10) {seconds = "0"+seconds}
 
-  return (hours > 0 ? hours + ':' : '') + minutes + ':' + seconds;
+  return (hours > 0 ? hours + ':' : '') + minutes + ':' + seconds
 }
 
 function update_timer() {
-  refresh_button.childNodes[0].innerHTML = refresh_text + ' (' + curent_time.toHHMMSS() + ')';
+  refresh_button.childNodes[0].innerText = refresh_text + ' (' + curent_time.toHHMMSS() + ')'
 
   if (curent_time == 0) {
     refresh()
   } else
-    curent_time--;
+    curent_time--
 }
 
 function refresh() {
-  b = Array.from(document.querySelectorAll('button')).find(el => el.textContent.search("Refresh") !== -1);
-
-  if (b != null) {
-    b.click();
-    curent_time = refresh_time;
-  }
+  refresh_button.click()
+  curent_time = refresh_time
 }
 
 function init() {
-  console.log("'Jira better auto refresh' is running")
-
-  refresh_button = Array.from(document.querySelectorAll('button')).find(el => el.textContent === 'Refresh');
+  refresh_button = document.querySelector("button.css-vl1vwy")
 
   if (refresh_button != null) {
-    refresh_text = refresh_button.childNodes[0].innerHTML;
+    refresh_text = refresh_button.childNodes[0].innerHTML
 
-    curent_time = refresh_time;
+    curent_time = refresh_time
 
-    setInterval(update_timer, 1000);
+    setInterval(update_timer, 1000)
 
     // reset timer on click
     refresh_button.onclick = function(e) {
-      curent_time = refresh_time;
+      curent_time = refresh_time
     }
 
     // refresh when hittint 'r' key
     document.onkeyup = function(e) {
     if (e.key == "r")
-      refresh();
+      refresh()
     }
-  }
-  else
+  } else {
     console.error("Refresh button not found")
+  }
 }
 
-setTimeout(init, 1000);
+setTimeout(init, 1000)
